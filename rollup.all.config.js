c283d8dd97ca9abfed45a505395e1b8a19131ed8
liyufei <liyufei@longfor.com>
2022-07-06 15:27:52 +0800

    feat:提取rollup公共配置

--- rollup.all.config.js
+++ rollup.all.config.js
@@ -1,10 +1,11 @@
 import * as path from "path";
+
 import baseConfig from './rollup.base.config.js'
 
 const outConfig = {
   output: [{
     dir: path.resolve(__dirname, 'dist/'),
-    entryFileNames: `mara.umd.js`,
+    entryFileNames: `mara.all.umd.js`,
     chunkFileNames: 'chunks/dep-[hash].js',
     format: 'umd',
     name: 'Mara',
@@ -13,7 +14,7 @@ const outConfig = {
     freeze: false
   }, {
     dir: path.resolve(__dirname, 'dist/'),
-    entryFileNames: `mara.esm.js`,
+    entryFileNames: `mara.all.esm.js`,
     chunkFileNames: 'chunks/dep-[hash].js',
     format: 'esm',
     name: 'Mara',
@@ -21,12 +22,15 @@ const outConfig = {
     externalLiveBindings: false,
     freeze: false
   }],
+ 
   external:id => {
-    return id.includes('node_modules')
+    // 需要把sweetalert2打包到库 进行es6转换
+    return id.includes('sweetalert2')?false:id.includes('node_modules')
   }
-
 };
 
+
 const config = Object.assign({},baseConfig, outConfig)
-// console.log('config1',config)
+// console.log('config2',config)
+
 export default config;
--- rollup.base.config.js
+++ rollup.base.config.js
@@ -7,31 +7,11 @@ import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload'
 import css from "rollup-plugin-import-css";
 import replace from '@rollup/plugin-replace';
-// import autoExternal from 'rollup-plugin-auto-external';
 
-const config = {
+const baseConfig = {
   input: {
     index: path.resolve(__dirname, 'src/index.js'),
   },
-  output: [{
-    dir: path.resolve(__dirname, 'dist/'),
-    entryFileNames: `mara.all.umd.js`,
-    chunkFileNames: 'chunks/dep-[hash].js',
-    format: 'umd',
-    name: 'Mara',
-    exports: 'default',
-    externalLiveBindings: false,
-    freeze: false
-  }, {
-    dir: path.resolve(__dirname, 'dist/'),
-    entryFileNames: `mara.all.esm.js`,
-    chunkFileNames: 'chunks/dep-[hash].js',
-    format: 'esm',
-    name: 'Mara',
-    exports: 'named',
-    externalLiveBindings: false,
-    freeze: false
-  }],
   treeshake: {
     moduleSideEffects: 'no-external',
     propertyReadSideEffects: false,
@@ -56,24 +36,18 @@ const config = {
     babel({
       babelHelpers: 'bundled',
       // exclude: "node_modules/**",
-      exclude:/node_modules\/(?!sweetalert2)/,
+      // exclude:/node_modules\/(?!sweetalert2)/,
     "presets": [
       ["@babel/preset-env"]],
     }),
     
     terser(),
-    // !process.argv.includes('-w') && autoExternal( ),
     process.argv.indexOf('-w') !== -1 && serve({
       open: true,
       port: 80,
       openPage: '/demo/demo.html',
     }),
   ],
-  external:id => {
-    // 需要把sweetalert2打包到库 进行es6转换
-    return id.includes('sweetalert2')?false:id.includes('node_modules')
-    
-  }
 };
 
-export default config;
+export default baseConfig;
