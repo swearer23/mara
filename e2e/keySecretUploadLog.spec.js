f9799cde239d7e024a135ccb4dd63f695089ebc0
yangshiwei <yangshiwei@longfor.com>
2022-07-25 12:00:09 +0800

    添加使用idm方式请求sts的e2e测试用例

--- e2e/keySecretUploadLog.spec.js
+++ e2e/keySecretUploadLog.spec.js
@@ -1,13 +1,13 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('default config for upload winerr log', async ({ page }) => {
+test('default config for apikey/secret way of uploading winerr log', async ({ page }) => {
   let reqCount = 0
-  await page.goto('http://localhost.longfor.com:8888/demo/demo.html');
+  await page.goto('http://localhost.longfor.com:8888/demo/demo.html?appid=945c1315b608be26bb32d15db8f6c806');
   page.on('request', req => {
     reqCount++
     if (reqCount === 1)
-      expect(req.url()).toContain('alioss/sts')
+      expect(req.url()).toContain('mara/alioss/sts')
     if (reqCount === 2)
       expect(req.url()).toContain('prod-zws-wuguofeng.oss-cn-beijing.aliyuncs.com')
   })
