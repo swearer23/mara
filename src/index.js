a7eeb9b19dd87402a286ac25164f90cb2ab6dfee
yangshiwei <yangshiwei@longfor.com>
2022-08-24 18:30:21 +0800

    fix bug typo

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ class Mara {
     traceIdKey = 'x-mara-trace-id',
     sessionIdKey = 'x-mara-session-id',
     slowAPIThreshold = 0,
-    excludeAjaxURL = undefined
+    excludeAjaxURL = []
   }) {
     this.checkParams(appname, appid)
     this.appname = appname
