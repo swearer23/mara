1b9dd9950a005c0708be664de282ac04ca255e95
yangshiwei <yangshiwei@longfor.com>
2022-09-26 18:04:35 +0800

    ignore JS_ERROR ResizeObserver loop limit exceeded

--- src/probe/winerr.js
+++ src/probe/winerr.js
@@ -2,6 +2,11 @@
 // https://w3c.github.io/html/webappapis.html#the-errorevent-interface
 // 探针
 
+const ignoredErrors = [
+  'ResizeObserver loop limit exceeded',
+  'ResizeObserver loop completed with undelivered notifications'
+]
+
 class WinErr {
   constructor (storage) {
     this.storage = storage
@@ -12,6 +17,9 @@ class WinErr {
     const { onerror } = window;
     window.onerror = (...args) => {
       if (typeof onerror === 'function') onerror.apply(this, args);
+      for (let i = 0; i < ignoredErrors.length; i++) {
+        if (args[0].indexOf(ignoredErrors[i]) === 0) return
+      }
       // probe(this.forms, message, url, line);
       this.pushLine(args[0], args[1], args[2], args[3], args[4]);
     };
